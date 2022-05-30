bf6f55774c5631395b3ab2166fd370d7c0d61e89
przypadek21 <mordalmichal@gmail.com>
2022-05-30 13:01:30 +0200

    123

--- tablice_rejestracyjne.py
+++ tablice_rejestracyjne.py
@@ -2,20 +2,21 @@ import random
 from itertools import product
 from string import ascii_lowercase
 
+
 class PlatesGenerator:
     def generate_plates(self):
         tablice_rejestracyjne = []
         litery = [''.join(i) for i in product(ascii_lowercase, repeat=3)]
-        cyfry = [random.randrange(0,10**4)]
+        cyfry = [random.randrange(0, 10 ** 4)]
         tablica = random.choice(litery) + str(random.choice(cyfry))
         while True:
             if tablica in tablice_rejestracyjne:
                 continue
             else:
                 tablice_rejestracyjne.append(tablica)
-            print(random.choice(tablice_rejestracyjne))
 
-pg= PlatesGenerator()
-while True:
-    print(pg.generate_plates())
+        return random.choice(tablice_rejestracyjne)
 
+
+pg = PlatesGenerator()
+pg.generate_plates()
