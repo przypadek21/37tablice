14547a3fe098c270efd12401ded11568414c3d38
przypadek21 <mordalmichal@gmail.com>
2022-05-31 18:37:08 +0200

    123

--- tablice_rejestracyjne.py
+++ tablice_rejestracyjne.py
@@ -5,23 +5,25 @@ from string import ascii_lowercase
 
 
 class PlatesGenerator:
-    def __init__(self, tablice_rejestracyjne):
-        self.tablice_rejestracyjne = tablice_rejestracyjne
+    def __init__(self):
+        self.tablice_rejestracyjne = []
     def generate_plates(self):
-        tablice_rejestracyjne = []
-        litery = [''.join(i) for i in product(ascii_lowercase, repeat=3)]
-        cyfry = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        tablica = random.choice(litery) + str(random.choice(cyfry)) + str(random.choice(cyfry)) + \
-                  str(random.choice(cyfry)) + str(random.choice(cyfry))
         while True:
-            if tablica in tablice_rejestracyjne:
-                continue
-            else:
-                tablice_rejestracyjne.append(tablica)
-                print(tablice_rejestracyjne)
+            tablice_rejestracyjne = []
+            litery = [''.join(i) for i in product(ascii_lowercase, repeat=3)]
+            cyfry = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+            tablica = random.choice(litery) + str(random.choice(cyfry)) + str(random.choice(cyfry)) + \
+                  str(random.choice(cyfry)) + str(random.choice(cyfry))
+
+            if tablica not in tablice_rejestracyjne:
+                self.tablice_rejestracyjne.append(tablica)
                 return tablica
 
+    def get_list(self):
+        return self.tablice_rejestracyjne
+
 #while True:
-pg = PlatesGenerator(tabice_rejestracyjne)
+pg = PlatesGenerator()
 print(pg.generate_plates())
+print(pg.get_list())
 
