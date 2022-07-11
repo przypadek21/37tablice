c1ceaf32c55b2c79c38eca77ec3f3b0f5874698b
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2022-07-11 13:48:29 +0200

    update test cases

--- tablice_rej/tablice_rejestracyjne.py
+++ tablice_rej/tablice_rejestracyjne.py
@@ -1,4 +1,3 @@
-import numbers
 import random
 from itertools import product
 from string import ascii_lowercase
@@ -7,9 +6,9 @@ from string import ascii_lowercase
 class PlatesGenerator:
     def __init__(self):
         self.tablice_rejestracyjne = []
+
     def generate_plates(self):
         while True:
-            tablice_rejestracyjne = []
             litery = [''.join(i) for i in product(ascii_lowercase, repeat=3)]
             cyfry = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
             tablica = random.choice(litery) + str(random.choice(cyfry)) + str(random.choice(cyfry)) + \
@@ -22,8 +21,5 @@ class PlatesGenerator:
     def get_list(self):
         return self.tablice_rejestracyjne
 
-
 pg = PlatesGenerator()
-print(pg.generate_plates())
 print(pg.get_list())
-
--- tablice_rej/test_tablice.py
+++ tablice_rej/test_tablice.py
@@ -1,35 +1,35 @@
 import unittest
 from tablice_rejestracyjne import PlatesGenerator
 
+platesToGenerate = 10
 
 class TestTablice(unittest.TestCase):
-
     @classmethod
-    def setUpClass(cls):
-        cls.plates = PlatesGenerator().generate_plates()
-        cls.plates_list = []
-        while len(cls.plates_list) < 5:
-            cls.plates_list.append(cls.plates)
-        cls.plates = PlatesGenerator().generate_plates()
+    def setUpClass(self):
+        self.pg = PlatesGenerator()
+        for _ in range(0, platesToGenerate):
+            self.pg.generate_plates()
 
-
-        print(cls.plates_list)
-
-
+    def test_get_plates(self):
+        self.assertEqual(self.pg.tablice_rejestracyjne, self.pg.get_list())
 
     def test_tablice_total(self):
-        self.assertEqual(len(self.plates), 7)
+        self.assertEqual(len(self.pg.tablice_rejestracyjne), platesToGenerate)
 
     def test_tablice_letters(self):
-        letters = sum(l.isalpha() for l in self.plates)
-        self.assertEqual(letters, 3)
+        for plate in self.pg.tablice_rejestracyjne:
+            letters = sum(l.isalpha() for l in plate)
+            self.assertEqual(letters, 3)
 
     def test_tablice_numbers(self):
-        numbers = sum(n.isdigit() for n in self.plates)
-        self.assertEqual(numbers, 4)
+        for plate in self.pg.tablice_rejestracyjne:
+            numbers = sum(n.isdigit() for n in plate)
+            self.assertEqual(numbers, 4)
 
     def test_tablice_differences(self):
-        last = self.plates_list[-1]
-        actual = self.plates
-        if len(self.plates_list) > 1:
-            self.assertNotEqual(last, actual)
+        alreadyExists = {}
+        for plate in self.pg.tablice_rejestracyjne:
+            self.assertTrue(plate not in alreadyExists)
+            alreadyExists[plate] = True
+if __name__ == '__main__':
+    unittest.main()
